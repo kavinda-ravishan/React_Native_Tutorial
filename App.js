4ede33a17c73d89fd5b9faa0c1d1dc9f4bda015e
kavinda ravishan <kvnravishan@outlook.com>
2021-04-08 17:35:30 +0530

    Platform: Get OS, StatusBar: get height of status bar

--- App.js
+++ App.js
@@ -1,29 +1,24 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import {
+  StyleSheet,
+  Button,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 
-//solve SafeAreaView apple top notch problem
-//<Image source={require("./assets/icon.png")} />
-//https://picsum.photos/id/237/200/300
-//TouchableWithoutFeedback : Make image Touchable
 export default function App() {
   return (
-    <View style={[styles.container, backgroundStyle]}>
-      <Text style={{ color: "red" }}>Inline Style</Text>
-      <Text style={textStyle}>Pass style as object</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title="Click Me" />
+    </SafeAreaView>
   );
 }
 
-const textStyle = { color: "green" };
-const backgroundStyle = { backgroundColor: "orange" };
-
-//StyleSheet.create validate props we pass Recomend to use
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
