5aca3c2afb323a30ac23bc9146b877e1885f94cd
kavinda ravishan <kvnravishan@outlook.com>
2021-04-08 16:32:35 +0530

    Text

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
+//solve SafeAreaView apple top notch problem
 export default function App() {
+  const handlePress = () => console.log("text pressed !");
+
+  //numberOfLines={1} wrap text on one line ...
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text} numberOfLines={1} onPress={handlePress}>
+        Open up App.js to start working on your app, Really Really long text
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+    color: "white",
+    fontWeight: "bold",
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "black",
     alignItems: "center",
     justifyContent: "center",
   },
