f4edfdaa0a7d291f19efb6179b764638fb8c15aa
kavinda ravishan <kvnravishan@outlook.com>
2021-04-08 17:06:13 +0530

    Button

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   View,
   Image,
   TouchableNativeFeedback,
+  Button,
 } from "react-native";
 
 //solve SafeAreaView apple top notch problem
@@ -15,23 +16,16 @@ import {
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
-      <TouchableNativeFeedback onPress={() => console.log("Image pressed")}>
-        <View
-          style={{ width: 200, height: 200, backgroundColor: "green" }}
-        ></View>
-      </TouchableNativeFeedback>
-      <StatusBar style="auto" />
+      <Button
+        title="Click Me"
+        onPress={() => console.log("Pressed")}
+        color="orange"
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    marginVertical: 20,
-  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
