04647277b140df7629096363b9676cd6a4edc140
kavinda ravishan <kvnravishan@outlook.com>
2021-04-08 16:52:25 +0530

    TouchableWithoutFeedback

--- App.js
+++ App.js
@@ -1,29 +1,43 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 //solve SafeAreaView apple top notch problem
 //<Image source={require("./assets/icon.png")} />
 //https://picsum.photos/id/237/200/300
+//TouchableWithoutFeedback : Make image Touchable
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Image
-        blurRadius={11}
-        fadeDuration={2000}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/id/237/200/300",
-        }}
-      />
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
+      <TouchableWithoutFeedback onPress={() => console.log("Image pressed")}>
+        <Image
+          blurRadius={11}
+          fadeDuration={2000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/id/237/200/300",
+          }}
+        />
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+    marginVertical: 20,
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
