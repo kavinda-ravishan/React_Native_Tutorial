18787780617c40179b2fd2b0b6dea0aae08b3236
kavinda ravishan <kvnravishan@outlook.com>
2021-04-08 22:27:17 +0530

    alignItems: baseline

--- App.js
+++ App.js
@@ -6,27 +6,33 @@ import { View } from "react-native";
 //flex default direction is column (can change by flexDirection: "row")
 //flexDirection: "row-reverse" items right to left
 //flexDirection: "column-reverse" items bottom to top
+
+//justifyContent: "center" align item in primery axis (flexDirection: "row" therefor primary axis (---))
+//justifyContent: "flex-start", default value
+//justifyContent: "space-evenly"
 export default function App() {
   return (
     <View
       style={{
         backgroundColor: "red",
         flex: 1,
-        flexDirection: "row",
+        flexDirection: "row", //main axis is horizontal
+        justifyContent: "center", //align item in main axis
+        alignItems: "baseline", //align item in secondary axis
       }}
     >
       <View
         style={{
           backgroundColor: "blue",
           width: 100,
-          height: 100,
+          height: 300,
         }}
       />
       <View
         style={{
           backgroundColor: "green",
           width: 100,
-          height: 100,
+          height: 200,
         }}
       />
       <View
